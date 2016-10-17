8cb53fb36c964cc8cdd04c83e20483a56a208125
Null_Pointer <liuxiaoming8839@sina.com>
2016-10-16 23:33:21 -0400

    Updated Global Variable Flags
    The protocal would stipulate first 5 characters of a message as the
    classification flag for the message so as to decide how the handler
    would handle the message.

--- GlobalVariables.java
+++ GlobalVariables.java
@@ -1,5 +1,19 @@
 
 public class GlobalVariables {
-	public static int HANDSHAKING_PORT = 6666;
-
+	public static final int HANDSHAKING_PORT = 6666;
+	public static final String REGISTER_ACTION = "reg_a";
+	public static final String DEREGISTER_ACTION = "der_a";
+	public static final String REGISTER_DENIED = "reg_d";
+	public static final String REGISTER_SUCCESS = "reg_s";
+	public static final String PUBLISH_ACTION = "pub_a";
+	public static final String PUBLISH_SUCCESS = "pub_s";
+	public static final String PUBLISH_DENIED = "pub_d";
+	public static final String IMFORMATION_REQUEST_ACTION = "inq_a";
+	public static final String IMFORMATION_REQUEST_SUCCESS = "inq_s";
+	public static final String USER_INFO_REQUEST_ACTION = "fiq_a";
+	public static final String USER_INFO_REQUEST_SUCCESS = "fiq_s";
+	public static final String USER_INFO_REQUEST_DENIED = "fiq_d";
+	public static final String REFER_ACTION = "ref_a";
+	public static final String CHAT_ACTION = "cha_a";
+	public static final String BYE_ACTION = "bye_a";
 }
